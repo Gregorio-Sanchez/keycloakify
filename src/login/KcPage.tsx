aeb3ecd51f27841134a076ec6cdadf2e707da6dc
Gregorio Sanchez <gregorio.sanchez@hefame.es>
2024-10-28 13:31:31 +0100

    cambio de dark a light

--- src/login/KcPage.tsx
+++ src/login/KcPage.tsx
@@ -83,7 +83,7 @@ function KcPageContextualized(props: { kcContext: KcContext }) {
 const useStyles = tss.create(({ theme }) => ({
     kcHtmlClass: {
         ":root": {
-            colorScheme: "dark"
+            colorScheme: "light"
         }
     },
     kcBodyClass: {
